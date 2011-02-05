4c5791ea6f04666edd5c1deff6f592bbdca20067
Christopher M Balz <christopherbalz@yahoo.com>
2011-02-04 19:13:22 -0800

    Added license.

--- js/Namespace.js
+++ js/Namespace.js
@@ -1,3 +1,24 @@
+/**
+ * This code sets the appropriate namespace and upper-level namespace values for the 'living_css_tools' package.  
+ * This code works with the 'add_proprietary_equivalents.sh' script in this repository, and also with the other classes 
+ * and files in this directory.
+ *
+ *   Copyright (C) 2011  Christopher M. Balz
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 self.living_css = {
     user_agent: { css: {} }
 };
