01f6645ec923ae72525b461ccd85420300cabf8f
Christopher M Balz <christopherbalz@yahoo.com>
2011-02-04 19:10:01 -0800

    Added license.

--- js/ChromeFrameVersionManager.js
+++ js/ChromeFrameVersionManager.js
@@ -1,3 +1,25 @@
+/**
+ *  Provides the capability to load the minimum version of Chrome Frame, or better, required to support the w3.org's
+ *  Flexible Box Module.  Differs from the standard Google Chrome Frame loader in that it can load the developer or 
+ *  beta channels of Chrome Frame.
+ *
+ *   Copyright (C) 2011  Christopher M. Balz
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+ 
 (function() {
     
      function ChromeFrameVersionManager() {
